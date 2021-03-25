249b30ac31b78c3e0739a75a45ead1d435ab8802
zezking <zhaoenze001@gmail.com>
2021-03-25 16:17:44 -0700

    finish

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Loading from "components/Loading";
 import Panels from "components/Panel";
 import classnames from "classnames";
+import axios from "axios";
 const data = [
   {
     id: 1,
@@ -26,13 +27,45 @@ const data = [
 ];
 
 class Dashboard extends Component {
-  state = { loading: false, focused: null };
+  state = {
+    loading: true,
+    focused: null,
+    days: [],
+    appointments: [],
+    interviewers: [],
+  };
 
   selectPanel(id) {
     this.setState((previousState) => ({
       focused: previousState.focused !== null ? null : id,
     }));
   }
+  componentDidMount() {
+    const focused = JSON.parse(localStorage.getItem("focused"));
+    Promise.all([
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers"),
+    ]).then(([days, appointments, interviewers]) => {
+      this.setState({
+        loading: false,
+        days: days.data,
+        appointments: appointments.data,
+        interviewers: interviewers.data,
+      });
+    });
+
+    if (focused) {
+      this.setState({ focused });
+    }
+  }
+
+  componentDidUpdate(previousProps, previousState) {
+    if (previousState.focused !== this.state.focused) {
+      localStorage.setItem("focused", JSON.stringify(this.state.focused));
+    }
+  }
+
   render() {
     const dashboardClasses = classnames("dashboard", {
       "dashboard--focused": this.state.focused,
